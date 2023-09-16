3c52f45b577afccfc0a2d28f363b98e1ceb5fe4e
ian <ian@ians-MacBook-Pro.local>
2023-09-16 12:25:58 +0300

    final build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
+import { notFound } from 'next/navigation'
 import React from 'react'
-async function wait(ms:number){
-  return new Promise(resolve=>setTimeout(resolve,ms))
-}
 
 async function page() {
-  await wait(10000)
-
-
   return (
-    <div>//// page</div>
+    notFound()
   )
 }
 
